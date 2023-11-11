0ee0fc1d61eeb0e7d2ba8f1b45b1716322fe6bf7
wldks5252 <63186821+wldks5252@users.noreply.github.com>
2023-11-11 17:13:51 +0900

    Update hw7.c

--- hw7.c
+++ hw7.c
@@ -4,31 +4,27 @@ int main(void) {
 	int arr1[6] = { 1,2,3,4,5,6 };
 	int arr2[6] = { 7,8,9,10,11,12 };
 
-	int* str1 = arr1;
-	int* str2 = arr2;
-
-	int i, temp;
+	int* ptr1 = arr1;
+	int* ptr2 = arr2;
+	int* temp;
+	int i;
 
 	printf("arr1 : ");
 	for (i = 0; i < 6; i++) 
-		printf("%d ", arr1[i]);
+		printf("%d ", ptr1[i]);
 	printf("\narr2 : ");
 	for (i = 0; i < 6; i++) 
-		printf("%d ", arr2[i]);
+		printf("%d ", ptr2[i]);
 
-	for (i = 0; i < 6; i++) {
-		temp = *str1;
-		*str1 = *str2;
-		*str2 = temp;
-		str1++;
-		str2++;
-	}
+	temp = ptr1;
+	ptr1 = ptr2;
+	ptr2 = temp;
 	
 	printf("\nafter swap");
 	printf("\narr1 : ");
 	for (i = 0; i < 6; i++)
-		printf("%d ", arr1[i]);
+		printf("%d ", ptr1[i]);
 	printf("\narr2 : ");
 	for (i = 0; i < 6; i++)
-		printf("%d ", arr2[i]);
+		printf("%d ", ptr2[i]);
 }
